0ad0be74391316a48a2adf4cb18ac7c7dd38cbd3
Velythyl <charlie.gauthier@umontreal.ca>
2021-11-01 14:37:15 -0400

    fixed thingy

--- run_many_runs.py
+++ run_many_runs.py
@@ -4,9 +4,17 @@ import sys
 
 
 def doit(root_dir, num_runs):
+    import subprocess, os
+    my_env = os.environ.copy()
+    my_env["PATH"] = "/usr/sbin:/sbin:" + my_env["PATH"]
+
     for run_id in range(int(num_runs)):
         run_root_dir = root_dir + str(run_id)
-        subprocess.check_output(f"python3 /data/paired_complexity_exps/clean-paired/social_rl/adversarial_env/train_adversarial_env.py --root_dir {run_root_dir}")
+        import io
+
+        proc = subprocess.Popen(f"python3 /data/paired_complexity_exps/clean-paired/social_rl/adversarial_env/train_adversarial_env.py --root_dir {run_root_dir}", env=my_env, shell=True, stdout=subprocess.PIPE)
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            print(line)
 
 if __name__ == "__main__":
     doit(sys.argv[1], sys.argv[2])
